f4879b3c6e81e1f0e0cfdcf2e70ec601995b03e3
lyubovmenshikova <dyachenko.lubov@yandex.ru>
2022-03-15 14:46:01 +0300

    New interface

---
+++ GCD/GCD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Lyubov Menshikova on 15.03.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ GCD/GCD/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Lyubov Menshikova on 15.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
